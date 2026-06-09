d65e93d5f16f1ae4b8c7263d44a7d6496c42b28f
OM M DODDAMANI <ommd3076@gmail.com>
2026-06-09 12:04:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Heap Sort/heap-sort.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void heapSort(int[] arr) {
+        int n = arr.length;
+        
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        
+        // Extract elements from heap one by one
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            
+            // Call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+    
+    private void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+        
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            
+            heapify(arr, n, largest);
+        }
+    }
+}
